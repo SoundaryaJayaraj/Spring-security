58a33cd63801a29df9991d1932b95fcd67d136e6
SoundaryaSFW <soundaryasfw@gmail.com>
2021-12-04 11:00:28 +0530

    initial commit

---
+++ src/main/java/com/springboot/springsecurity/demo/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.springsecurity.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.springsecurity.demo.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
